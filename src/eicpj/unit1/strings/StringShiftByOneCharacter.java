661a1087bcfa2d5e3fd685cedb4e7b307b40d721
Alberto Vidana <alberto.vidana@gmail.com>
2025-03-04 22:29:35 -0600

    String shifting one posiion to the ->
    Practice and solution of Easy interview coding practice in Java

---
+++ src/eicpj/unit1/strings/StringShiftByOneCharacter.java
@@ -0,0 +1,186 @@
+/*
+ * OUTPUT
+ * 
+> Task :test
+
+SolutionTest > test10() PASSED
+
+SolutionTest > test1() PASSED
+
+SolutionTest > test2() PASSED
+
+SolutionTest > test3() PASSED
+
+SolutionTest > test4() PASSED
+
+SolutionTest > test5() PASSED
+
+SolutionTest > test6() PASSED
+
+SolutionTest > test7() PASSED
+
+SolutionTest > test8() PASSED
+
+SolutionTest > test9() PASSED
+
+BUILD SUCCESSFUL in 1s
+3 actionable tasks: 3 executed
+
+
+OR with bad solution
+
+
+> Task :test
+
+SolutionTest > test10() FAILED
+    org.opentest4j.AssertionFailedError: expected: <beA$56Z> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test10(SolutionTest.java:55)
+
+SolutionTest > test1() FAILED
+    org.opentest4j.AssertionFailedError: expected: <bcd123YZa!> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test1(SolutionTest.java:10)
+
+SolutionTest > test2() FAILED
+    org.opentest4j.AssertionFailedError: expected: <Ifmmp, Xpsme!> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test2(SolutionTest.java:15)
+
+SolutionTest > test3() FAILED
+    org.opentest4j.AssertionFailedError: expected: <9876543210> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test3(SolutionTest.java:20)
+
+SolutionTest > test4() FAILED
+    org.opentest4j.AssertionFailedError: expected: <oiqr$%FQW45KA> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test4(SolutionTest.java:25)
+
+SolutionTest > test5() FAILED
+    org.opentest4j.AssertionFailedError: expected: <Qzuipojtdppm> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test5(SolutionTest.java:30)
+
+SolutionTest > test6() FAILED
+    org.opentest4j.AssertionFailedError: expected: <Afcsb> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test6(SolutionTest.java:35)
+
+SolutionTest > test7() FAILED
+    org.opentest4j.AssertionFailedError: expected: <JoufswjfxQsfqbsbujpo> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test7(SolutionTest.java:40)
+
+SolutionTest > test8() FAILED
+    org.opentest4j.AssertionFailedError: expected: <ZXYza> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test8(SolutionTest.java:45)
+
+SolutionTest > test9() FAILED
+    org.opentest4j.AssertionFailedError: expected: <tijgu123> but was: <newS>
+        at app//org.junit.jupiter.api.AssertionUtils.fail(AssertionUtils.java:55)
+        at app//org.junit.jupiter.api.AssertionUtils.failNotEqual(AssertionUtils.java:62)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:182)
+        at app//org.junit.jupiter.api.AssertEquals.assertEquals(AssertEquals.java:177)
+        at app//org.junit.jupiter.api.Assertions.assertEquals(Assertions.java:1125)
+        at app//SolutionTest.test9(SolutionTest.java:50)
+There were failing tests. See the report at: file:///usercode/FILESYSTEM/build/reports/tests/test/index.html
+
+BUILD SUCCESSFUL in 1s
+3 actionable tasks: 3 executed
+
+ERRORS
+10 tests completed, 10 failed
+
+TERMINAL
+/usercode/FILESYSTEM$ bash /usercode/FILESYSTEM/.codesignal/setup.sh -a
+
+Welcome to Gradle 8.9!
+
+Here are the highlights of this release:
+ - Enhanced Error and Warning Messages
+ - IDE Integration Improvements
+ - Daemon JVM Information
+
+For more details see https://docs.gradle.org/8.9/release-notes.html
+
+Starting a Gradle Daemon (subsequent builds will be faster)
+:compileJava SKIPPED
+:processResources SKIPPED
+:classes SKIPPED
+:jar SKIPPED
+:assemble SKIPPED
+:compileTestJava SKIPPED
+:processTestResources SKIPPED
+:testClasses SKIPPED
+:test SKIPPED
+:check SKIPPED
+:build SKIPPED
+
+BUILD SUCCESSFUL in 9s
+/usercode/FILESYSTEM$ 
+
+
+ */
+
+package eicpj.unit1.strings;
+
+public class StringShiftByOneCharacter {
+
+    public String shiftByOneCharacter(String s) {
+        // TODO: Implement the solution here
+        String newS = "";
+        
+        for (char c: s.toCharArray()) {
+            if (c >= 'a' && c <= 'y') {
+                newS += (char) (c + 1);
+            } else if (c == 'z') {
+                newS += 'a';
+            } else if (c >= 'A' && c <= 'Y') {
+                newS += (char) (c + 1);
+            } else if(c == 'Z') {
+                newS += 'A';
+            } else {
+                newS += (char) c;
+            }
+        }
+        return newS;
+    }
+}
