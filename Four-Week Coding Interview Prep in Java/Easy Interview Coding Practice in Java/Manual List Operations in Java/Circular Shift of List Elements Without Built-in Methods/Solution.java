ae802bf9607a79806be1b2a37e791d9cc1c8e7cf
Alberto Vidana <alberto.vidana@gmail.com>
2025-09-03 23:38:44 -0600

    #CodeSignal :: #Four-Weeks :: #Java #ManulaListOperations :: Continue with this course and target to complete before the end of year.

---
+++ Four-Week Coding Interview Prep in Java/Easy Interview Coding Practice in Java/Manual List Operations in Java/Circular Shift of List Elements Without Built-in Methods/Solution.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Solution {
+    public List<Integer> shiftListElements(List<Integer> ls, int shift) {
+        // TODO: Implement the solution
+        return null;
+    }
+}
