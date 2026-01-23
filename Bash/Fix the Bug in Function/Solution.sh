ef70dba4e9472ae2c80f9fc22e9fb1d03e775b6a
Alberto Vidana <alberto.vidana@gmail.com>
2026-01-23 00:02:55 -0600

    #CodeSignal #Bash :: Introduction to Shell Scripting Basics #Unit6

---
+++ Bash/Fix the Bug in Function/Solution.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+update_apps() {
+  # TODO: Convert the arguments to an array and assign it to the variable apps
+  apps=_______
+  
+  # TODO: Loop through the "apps" array
+  for app in "_______"
+  do
+    echo "Checking $app"
+  done
+  
+  # TODO: Loop through the positional arguments without using the "apps" array
+  for app in "______"
+  do
+    echo "Updating $app"
+  done
+}
+
+update_apps "Photos" "Browser" "Documents"
